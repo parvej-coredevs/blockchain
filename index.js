d8a332f4f7b43903dc778f2a09085e0ca7c381fd
Parvej Hossain <parvej_hossain@coredevs.ltd>
2023-12-11 19:26:33 +0600

    test blockchaiun

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import config from './config';
-import App from './app';
+import { ethers, JsonRpcProvider } from 'ethers';
+
 
 (async () => {
+  const provider = new JsonRpcProvider(`http://127.0.0.1:7545`);
 
-  const app = new App(config);
-  await app.init();
+  // accounts list
+  const accounts =  await provider.listAccounts();
+  // accounts.forEach(acc => console.log(acc.address));
 
+  // get account balance
+  const balance = await provider.getBalance('0x2a9392ea4b5ddC5257063f328bAa31a306bce650');
+  console.log('balance: ' + balance);
 
 })()
--- services/index.js
+++
@@ -1,5 +0,0 @@
-
-
-export default function (app) {
-  app.implements();
-}
