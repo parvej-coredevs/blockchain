4092c6596d217c9d8c6cb376eb6907f0ae985ba6
Parvej Hossain <parvej_hossain@coredevs.ltd>
2023-12-11 14:42:43 +0600

    feat: starting blockchaing framework

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+export default class App {
+
+  constructor(config) {
+    this.config = config;
+
+    console.log('[+] Initializing App');
+  }
+
+  async init() {
+    // initialize wallet here
+    
+
+    await this.start();
+  }
+
+  async start() {
+
+  }
+
+  attach() {
+
+  }
+}
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
+import config from './config';
+import App from './app';
 
 (async () => {
+
+  const app = new App(config);
+  await app.init();
+
 
 })()
